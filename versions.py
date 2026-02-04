f456ea1b76a7b62be81dcc9a3b8e20fa05388b0b
Bhagwan01 <shivammishra242424@gmail.com>
2026-02-04 21:47:24 +0530

    added notebooks

---
+++ versions.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib
+import seaborn as sns
+import pandas as pd
+import sklearn
+import xgboost as xgb
+import catboost
+numpy_verion='1.24.4'
+matplot='3.7.5'
+sns='0.13.2'
+panda='2.0.3'
+sklaern='1.3.2'
+xgboo='2.1.4'
+catboost='1.2.8'
+
+print(np.__version__)
+print(matplotlib.__version__)
+print(sns.__version__)
+print(pd.__version__)
+print(sklearn.__version__)
+print(xgb.__version__)
+print(catboost.__version__)
